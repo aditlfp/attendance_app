4992d65235b4492a878a52fc87b783d568f16be7
aditlfp <aditya.budilfp11@gmail.com>
2025-09-09 20:32:10 +0700

    feat: logic face recognition implement

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZlOU8lMyhd7a-4qI9vKGB_Ib6kjopBCs",
-  authDomain: "auth-flutter-814b8.firebaseapp.com",
-  projectId: "auth-flutter-814b8",
-  storageBucket: "auth-flutter-814b8.firebasestorage.app",
-  messagingSenderId: "532372392693",
-  appId: "1:532372392693:web:85bce9326de0a61bc053bc"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
